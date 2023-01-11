54b602ae92282c326276d05d25daa9ce4b3af1b4
perditavojo <117562794+perditavojo@users.noreply.github.com>
2023-01-11 22:32:56 +0800

    feat: 套用數個異動
    1. 排除追蹤 *.jks 檔案。
    2. 更新 Flutter 函式庫。
    3. 更新 Android 用的函式庫。
    4. 配合函式庫的 BREAKING CHANGE 修正程式碼。

--- lib/widgets/webview_stack.dart
+++ lib/widgets/webview_stack.dart
@@ -3,6 +3,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
+import 'package:webview_flutter_web/webview_flutter_web.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class WebViewStack extends StatefulWidget {
   const WebViewStack({Key? key, required this.url}) : super(key: key);
@@ -14,100 +19,111 @@ class WebViewStack extends StatefulWidget {
 }
 
 class _WebViewStackState extends State<WebViewStack> {
+  late final WebViewController _controller;
+  late final PlatformWebViewController _controllerWeb;
+
   int loadingPercentage = 0;
 
   @override
-  Widget build(BuildContext context) {
-    @override
-    void initState() {
-      super.initState();
+  void initState() {
+    super.initState();
 
-      if (Platform.isAndroid) {
-        WebView.platform = SurfaceAndroidWebView();
+    if (kIsWeb) {
+      WebViewPlatform.instance = WebWebViewPlatform();
+
+      final PlatformWebViewController controllerWeb = PlatformWebViewController(
+        const PlatformWebViewControllerCreationParams(),
+      )..loadRequest(
+          LoadRequestParams(
+            uri: Uri.parse(widget.url),
+          ),
+        );
+
+      _controllerWeb = controllerWeb;
+    } else {
+      late final PlatformWebViewControllerCreationParams params;
+
+      if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+        params = WebKitWebViewControllerCreationParams(
+          allowsInlineMediaPlayback: true,
+          mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+        );
+      } else {
+        params = const PlatformWebViewControllerCreationParams();
       }
-    }
 
-    final Completer<WebViewController> controller =
-        Completer<WebViewController>();
+      final WebViewController controller =
+          WebViewController.fromPlatformCreationParams(params);
 
-    JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
-      return JavascriptChannel(
-          name: 'Toaster',
-          onMessageReceived: (JavascriptMessage message) {
+      controller
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setBackgroundColor(const Color(0x00000000))
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onProgress: (int progress) {
+              setState(() {
+                loadingPercentage = progress;
+              });
+            },
+            onPageStarted: (String url) {
+              setState(() {
+                loadingPercentage = 0;
+              });
+            },
+            onPageFinished: (String url) {
+              setState(() {
+                loadingPercentage = 100;
+              });
+            },
+            onWebResourceError: (WebResourceError error) {
+              String errMsg = '''
+                頁面資源錯誤：
+                錯誤碼：${error.errorCode}
+                描述：${error.description}
+                錯誤類玲：${error.errorType}
+                是否來自主框架：${error.isForMainFrame}
+              ''';
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(errMsg)),
+              );
+            },
+            onNavigationRequest: (NavigationRequest request) {
+              return NavigationDecision.navigate;
+            },
+          ),
+        )
+        ..addJavaScriptChannel(
+          'Toaster',
+          onMessageReceived: (JavaScriptMessage message) {
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(content: Text(message.message)),
             );
-          });
+          },
+        )
+        ..loadRequest(Uri.parse(widget.url));
+
+      if (controller.platform is AndroidWebViewController) {
+        AndroidWebViewController.enableDebugging(true);
+        (controller.platform as AndroidWebViewController)
+            .setMediaPlaybackRequiresUserGesture(false);
+      }
+
+      _controller = controller;
     }
+  }
 
-    WebView webView = WebView(
-      initialUrl: widget.url,
-      javascriptMode: JavascriptMode.unrestricted,
-      javascriptChannels: <JavascriptChannel>{
-        _toasterJavascriptChannel(context),
-      },
-      onWebViewCreated: (WebViewController webViewController) {
-        controller.complete(webViewController);
-      },
-      onPageStarted: (url) {
-        setState(() {
-          loadingPercentage = 0;
-        });
-      },
-      onProgress: (progress) {
-        setState(() {
-          loadingPercentage = progress;
-        });
-      },
-      onPageFinished: (url) {
-        setState(() {
-          loadingPercentage = 100;
-        });
-      },
-    );
-
-    Widget goBackButton() {
-      return FutureBuilder<WebViewController>(
-        future: controller.future,
-        builder: (BuildContext context,
-            AsyncSnapshot<WebViewController> controller) {
-          return FloatingActionButton(
-            tooltip: '返回',
-            onPressed: () async {
-              bool showErrorMessage = true;
-
-              if (controller.hasData) {
-                bool? canGoBack = await controller.data?.canGoBack();
-
-                if (canGoBack != null && canGoBack) {
-                  showErrorMessage = false;
-
-                  await controller.data?.goBack();
-                }
-              }
-
-              if (mounted && showErrorMessage) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text(
-                      '目前無歷史紀錄可以返回。',
-                    ),
-                  ),
-                );
-              }
-            },
-            child: const Icon(
-              Icons.arrow_back,
-            ),
-          );
-        },
-      );
-    }
-
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          webView,
+          kIsWeb
+              ? PlatformWebViewWidget(
+                  PlatformWebViewWidgetCreationParams(
+                      controller: _controllerWeb),
+                ).build(context)
+              : WebViewWidget(controller: _controller),
           if (loadingPercentage < 100)
             LinearProgressIndicator(
               value: loadingPercentage / 100.0,
@@ -116,6 +132,35 @@ class _WebViewStackState extends State<WebViewStack> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: goBackButton(),
+    );
+  }
+
+  Widget goBackButton() {
+    return FloatingActionButton(
+      tooltip: '返回',
+      onPressed: () async {
+        bool showErrorMessage = true;
+        bool canGoBack = await _controller.canGoBack();
+
+        if (canGoBack) {
+          showErrorMessage = false;
+
+          await _controller.goBack();
+        }
+
+        if (mounted && showErrorMessage) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                '目前無歷史紀錄可以返回。',
+              ),
+            ),
+          );
+        }
+      },
+      child: const Icon(
+        Icons.arrow_back,
+      ),
     );
   }
 }
